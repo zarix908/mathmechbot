4cde9c01f22345863cb7c5cf2233d1f1141feb8b
zarix908 <daniyarrus@gmail.com>
2019-04-21 01:56:43 +0500

    add authentication

--- telegram_bot.py
+++ telegram_bot.py
@@ -15,19 +15,28 @@ class TelegramBot(telebot.TeleBot):
     def process_update(self, update):
         try:
             message = self.parse_message(update)
-            self.process_message(message)
+            chat_id = self.parse_chat_id(update)
+
+            if not message.startswith('/'):
+                return
+
+            if str(chat_id) != config.TELEGRAM_DIALOG_ID:
+                self.send_message(chat_id, 'Ошибка прав доступа')
+                return
+
+            self.process_message(message, chat_id)
         except Exception:
             self.send_message(config.TELEGRAM_DIALOG_ID, "Ой, ошибочка вышла")
 
     @log
-    def process_message(self, message):
+    def process_message(self, message, chat_id):
         if message == '/show':
-            self.show_new_posts()
+            self.show_new_posts(chat_id)
         if message == '/post':
-            self.repost()
+            self.repost(chat_id)
 
     @log
-    def show_new_posts(self):
+    def show_new_posts(self, chat_id):
         if self.__posted:
             self.__posts_manager.update()
             self.__posted = False
@@ -35,14 +44,14 @@ class TelegramBot(telebot.TeleBot):
         posts = self.__posts_manager.posts
 
         if len(posts) == 0:
-            self.send_message(config.TELEGRAM_DIALOG_ID, 'Новых постов нет')
+            self.send_message(chat_id, 'Новых постов нет')
 
         for post in posts:
             if post.text:
-                self.send_message(config.TELEGRAM_DIALOG_ID, post.text)
+                self.send_message(chat_id, post.text)
 
     @log
-    def repost(self):
+    def repost(self, chat_id):
         posts = self.__posts_manager.posts
         posts.sort(key=lambda el: el.id)
 
@@ -51,8 +60,15 @@ class TelegramBot(telebot.TeleBot):
                 self.send_message(config.TELEGRAM_CHANNEL_ID, post.text)
 
         self.__posted = True
-        self.send_message(config.TELEGRAM_DIALOG_ID, 'Посты опубликованы')
+        self.send_message(chat_id, 'Посты опубликованы')
 
     @log
     def parse_message(self, update):
         return json.loads(update)['message']['text']
+
+    @log
+    def parse_chat_id(self, update):
+        update = json.loads(update)
+
+        if 'message' in update:
+            return update['message']['chat']['id']
